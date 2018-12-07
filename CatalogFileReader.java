1af4dc0a133bda5c1d8c4af750b8abee2b940506
Katherine Zhou <katherine@Katherines-MacBook-Pro-2.local>
2018-12-06 19:21:53 -0500

    updated gui and analysis

--- CatalogFileReader.java
+++ CatalogFileReader.java
@@ -1,42 +1,68 @@
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class CatalogFileReader {
 	
 	File fileName;
 
-	public CatalogFileReader(String fileName){
+	public CatalogFileReader(String fileName) throws FileNotFoundException{
 		this.fileName = new File(fileName);
 	}
 
 	public ArrayList<Artwork> readCatalog(){		
 		ArrayList<Artwork> aws = new ArrayList<Artwork>();
 		try {
+		
 			Scanner scan = new Scanner(this.fileName);
 			scan.nextLine();
 			while(scan.hasNextLine()){
 					String line = scan.nextLine();
-					String[] parts = line.split("\t");
-				    String author = parts[0];
-				    String lifespan = parts[1];
-				    String title = parts[2];
-				    String date = parts[3];
-				    String technique = parts[4];
-				    String location = parts[5];
-				    String url = parts[6];
-				    String form = parts[7];
-				    String type = parts[8];
-				    String school = parts[9];
-				    String timeframe = parts[10];
+					String[] parts = line.split(",");
+					ArrayList<String> s = new ArrayList<String>();	
+					int i = 0;
+					while(i<parts.length) {				
+						if( parts[i].charAt(0)=='"' && parts[i].charAt(parts[i].length()-1)!='"'){
+							int j = i+1;
+							
+								while( parts[j].charAt(parts[j].length()-1)!= '"') {
+									parts[i]=parts[i]+","+parts[j];
+									j++;												
+						}
+								String q = parts[i]+","+parts[j];
+								s.add(q.substring(1, q.length()-1));
+								i=j+1;
+							
+						}else {
+						s.add(parts[i]);	
+						i++;
+						}			
+					}				
+					
+				    String author = s.get(0);				    
+				    String lifespan = s.get(1);
+				    String title = s.get(2);			   
+				    String date = s.get(3);
+				    String technique = s.get(4);
+				    String location = s.get(5);		
+				    String url = s.get(6);
+				    String form = s.get(7);
+				    String type = s.get(8);
+				    String school = s.get(9);
+				    String timeframe = s.get(10);
+				    
 				    Artwork aw = new Artwork(author, lifespan, title, date, technique, location, url, form, type, school, timeframe);
 				   aws.add(aw);
 			}
 			scan.close();
-			
+			//System.out.println(aws.get(29208).getAuthor()+aws.get(29208).getDate()+aws.get(29208).getForm()+aws.get(29208).getLocation());
 	}catch(IOException e){
 		System.out.println("Error reading file ");
 		e.printStackTrace();
--- Gui.java
+++ Gui.java
@@ -1,24 +1,154 @@
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+
+import javafx.scene.control.Hyperlink;
+/**
+ * 
+ * @author keqin zhou
+ *
+ */
 
 public class Gui {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Artwork Searcher");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 300);
+	
+	public static void main(String[] args) throws IOException {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(700,400);		
+		frame.setTitle("Art Searcher");	
+	
 		JPanel panel = new JPanel();
 		
-		JLabel label = new JLabel("Enter Artist");
-		JTextField tf = new JTextField(9);
-		JButton button = new JButton("Search");
+		JLabel label = new JLabel("The Name of Artist:");	
+		JTextField textField = new JTextField(20);
+		JButton button = new JButton("Search");	
+		String imageLocation = "Water_Lilies.jpg";
+		BufferedImage pic = ImageIO.read(new File(imageLocation));
+		JLabel picture = new JLabel(new ImageIcon(pic));
+		
+		
 		panel.add(label);
-		panel.add(tf);
-		panel.add(button);
-		frame.getContentPane().add(panel);
-		frame.setVisible(true);
+		panel.add(textField);
+		panel.add(button); 
+		panel.add(picture);
+		
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		CatalogFileReader cfr = new CatalogFileReader("acatalog.csv");
+		ArrayList<Artwork> aws = cfr.readCatalog();
+		ArtAnalysis aa = new ArtAnalysis(aws);
+
+	
+	    button.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String author = textField.getText().trim();
+			ArrayList<String> ts = new ArrayList<String>();
+			ts.addAll(aa.getTitle(author));	
+			
+			
+			JFrame frame1 = new JFrame();
+			frame1.setSize(700,400);		
+			frame1.setTitle("Artworks");
+		
+			JPanel panel1 = new JPanel();
+			Box box = Box.createVerticalBox();
+			ArrayList<JButton> jls = new ArrayList<JButton>();
+			for(int i=0;i<ts.size();i++) {
+			JButton jl = new JButton(ts.get(i));
+			jls.add(jl);
+			box.add(jl);
+			}
+			JScrollPane scrollPane = new JScrollPane(box);
+			JScrollBar vbar=new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 500);
+			JButton b = new JButton("Where are they");	
+			panel1.add(b);
+			frame1.add(scrollPane);
+			frame1.add(vbar, BorderLayout.EAST);
+			frame1.add(box);
+			frame1.setVisible(true);
+			frame1.pack();
+			frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			for(int i=0;i<ts.size();i++) {
+		     String title = ts.get(i);
+			 jls.get(i).addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+			
+	        ArrayList<String> info = new ArrayList<String>();
+	   
+			info = aa.getInfo(title); 
+			String url = info.get(5);
+			url = url.replaceFirst("html","art");
+			url = url.replaceFirst("html","jpg");
+
+			
+			URL img = null;
+			try {
+				img = new URL(url);
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+			}
+			
+			ImageIcon image = new ImageIcon(img);
+			JLabel p = new JLabel("", image, JLabel.CENTER);
+			
+			JFrame frame2 = new JFrame();
+			frame2.setSize(400,400);
+			frame2.setTitle("Info and Location");
+			JPanel panel2 = new JPanel();
+			JLabel label3 = new JLabel("<html>"+
+			"Title: "+title+"<br/>"+
+			"Artist: "+author+"<br/>"+
+			"Lifespan: "+info.get(5)+"<br/>"+
+			"Date: "+info.get(1)+"<br/>"+
+			"School: "+info.get(2)+"<br/>"+
+			"Technique: "+info.get(3)+"<br/>"+
+			"Type: "+info.get(4)+"<br/>"+
+			"Form: "+info.get(6)+"<br/>"+
+			"Location: "+info.get(0)+"<br/>"+
+			"<html>");			
+			panel2.add(p);
+			panel2.add(label3);	
+			frame2.add(panel2);
+			frame2.setVisible(true);
+			frame2.pack();
+			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			
+		}
+	});
+			}
+				
+		}
+		});
 	}
+	  
+	
 }
